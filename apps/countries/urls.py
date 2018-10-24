5571507ea6932ae6e9c03c75b9e70ee5b95b8c10
Fernando Portilla <fernandoportilla1@gmail.com>
2018-10-24 16:20:28 -0500

    city list

--- apps/countries/urls.py
+++ apps/countries/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CountryListView, CountryCreateView
+from .views import CountryListView, CountryCreateView, CityListView
 
 urlpatterns = [
-    path('country', CountryListView.as_view(), name='country-list'),
-    path('country/create', CountryCreateView.as_view(), name='country-create'),
+    path('country', CountryListView.as_view(), name='country-list-view'),
+    path('country/create', CountryCreateView.as_view(), name='country-create-view'),
+    path('city', CityListView.as_view(), name='city-list-view'),
 ]
--- apps/countries/views.py
+++ apps/countries/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
 
 from apps.countries.forms import CountryForm
-from apps.countries.models import Country
+from apps.countries.models import Country, City
 
 
 # Create your views here.
@@ -11,8 +11,28 @@ from apps.countries.models import Country
 class CountryListView(ListView):
     model = Country
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_country'] = 'active'
+        return context
+
 
 class CountryCreateView(CreateView):
     model = Country
     form_class = CountryForm
     success_url = reverse_lazy('country-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_country'] = 'active'
+        return context
+
+
+class CityListView(ListView):
+    model = City
+    template_name = 'cities/city_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_city'] = 'active'
+        return context
